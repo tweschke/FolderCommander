ef9715c3e5e8c09c69befd196d96e78a9044f71e
Thomas W <65774908+tweschke@users.noreply.github.com>
2026-01-09 17:22:41 +0000

    Initial Commit

---
+++ Folder Commander/Folder_CommanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Folder_CommanderApp.swift
+//  Folder Commander
+//
+//  Created by Thomas Weschke on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Folder_CommanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Folder CommanderTests/Folder_CommanderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Folder_CommanderTests.swift
+//  Folder CommanderTests
+//
+//  Created by Thomas Weschke on 09/01/2026.
+//
+
+import Testing
+@testable import Folder_Commander
+
+struct Folder_CommanderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
